4c66a5cef45fe1ff1d32961ee94d6fef17f01276
indarjoko <ijpdesigner@yandex.com>
2024-10-31 13:53:11 +0700

    updated

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,7 +8,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/itbbg/itguide',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'IT Documentation Guide',
   },
 }
 
